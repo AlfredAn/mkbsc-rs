91560ce900976448da6ecf92ba8127f5d6908783
Rygno <alfred@rygno.net>
2023-02-17 18:06:28 +0100

    add failing test case

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ mod io;
 mod cli;
 
 fn main() -> anyhow::Result<()> {
-    run_cli()?;
+    //run_cli()?;
+    debug()?;
     Ok(())
 }
 
@@ -20,4 +21,30 @@ fn run_cli() -> anyhow::Result<()> {
     cli::run(&cli)?;
 
     Ok(())
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_cart_pushing_game() {
+        fn load_game() -> Game<2> {
+            let g = include_game!("../games/cart_pushing", 2).build().game;
+            let mut stack = MKBSCStack::new(g);
+            for _ in 0..6 {
+                stack.push();
+            }
+            let mut g = stack.last().game().as_ref().clone();
+            g.origin = None;
+            g
+        }
+
+        let g0 = load_game();
+        println!("{}\n", g0.loc.len());
+
+        for _ in 0..50 {
+            let g = load_game();
+            assert!(is_isomorphic(&g0, &g, true));
+        }
+    }
 }
